a4b795339f8d421fb36a586b37cb13ed8a358bea
norhan-nabil <nabilnorhan324@gmail.com>
2025-08-12 17:25:03 +0300

    test: JSON serialization/deserialization testing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Todo_CLI_App.Models;
+using Todo_CLI_App.Services;
+
+class Program
+{
+    static async Task Main()
+    {
+        // Quiq test for storage
+        string filePath = "tasks.json";
+        var storage = new JsonTaskStorage(filePath);
+
+        
+        var tasks = new List<TodoItem>
+        {
+            new TodoItem("Buy milk", Priority.High)
+            { Tags = new List<string> { "shopping" } },
+            new TodoItem("Study C#", Priority.Medium) { Tags = new List<string> { "learning", "coding" } }
+        };
+
+ 
+        await storage.SaveAsync(tasks);
+        Console.WriteLine("✅ Tasks saved!");
+
+
+        var loadedTasks = await storage.LoadAsync();
+        Console.WriteLine("📋 Loaded tasks:");
+        foreach (var task in loadedTasks)
+        {
+            Console.WriteLine(task.Description);
+        }
+    }
+}
