7a591eafce616b0c60345f7c94684d01d0a30fee
norhan-nabil <nabilnorhan324@gmail.com>
2025-08-12 18:01:37 +0300

    test: test all ToDoManager operations

--- Program.cs
+++ Program.cs
@@ -5,6 +5,26 @@ class Program
 {
     static async Task Main()
     {
+        var storage = new JsonTaskStorage("tasks.json"); 
+        var manager = new TodoManager(storage);
 
+        await manager.InitializeAsync();
+
+  
+       // var newTask = await manager.AddAsync("Buy milkmmmmmmmm", Priority.High, new List<string> { "shopping", "urgent" });
+     //   Console.WriteLine($"Added: {newTask.Description} with ID {newTask.Id}");
+
+  
+        var allTasks = manager.GetAll();
+        foreach (var task in allTasks)
+        {
+            Console.WriteLine($"{task.Id}: {task.Description} - Completed: {task.IsCompleted}");
+        }
+
+
+       // await manager.MarkDoneAsync(1);
+
+
+        await manager.DeleteAsync(1);
     }
 }
